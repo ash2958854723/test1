3e9158a1716215ea5496dbd3e614be0177a53388
Your Name <email@example.com>
2017-05-02 14:12:16 +0800

    Test

---
+++ src/efeer/Ffds.java
@@ -0,0 +1,15 @@
+package efeer;
+
+public class Ffds {
+
+	public static void main(String[] args) {
+		
+			
+				int a=10;
+				System.out.println(a);
+			}
+
+		
+
+	}
+
---
+++ src/efeer/SSS.java
@@ -0,0 +1,6 @@
+package efeer;
+
+public class SSS {
+  byte b=3;
+  int x=b;
+}
